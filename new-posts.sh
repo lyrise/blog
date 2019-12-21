61e5b5615ab53007a46d34ff8ecb4220f2be795a
Lyrise <lyrise1984@gmail.com>
2019-12-21 21:08:06 +0900

    Fix 加筆

--- new-posts.sh
+++ new-posts.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-read s Y m d H M S ms ns <<< "$(date +'%s %Y %m %d %H %M %S %3N %9N')"
-
-hugo new "posts/${Y}-${m}-${d}_${H}-${M}-${S}/index.md"
+hugo new "posts/$(date '+%Y-%m-%d_%H-%M-%S')/index.md"
